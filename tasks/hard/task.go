17ab94c79235dbdd0c6671ce52ffbd6a5fbf775c
madiyor-abdukhashimov <madiyor.abdukhashimov@global-fashion-group.com>
2023-02-24 10:10:34 +0500

    Add examples

--- tasks/hard/task.go
+++ tasks/hard/task.go
@@ -8,6 +8,16 @@ The command consists of an alphabet of "G", "()" and/or "(al)" in some order.
 The Goal Parser will interpret
 "G" as the string "G", "()" as the string "o", and "(al)" as the string "al".
 The interpreted strings are then concatenated in the original order.
+
+For example:
+input: "G()(al)
+output: Goal
+
+input: G()()()()(al)
+output: Gooooal
+
+input: (al)G(al)()()G
+output: alGalooG
 */
 func GoalParsers(strReader *strings.Reader) string {
 	panic("Not implemented yet...")
